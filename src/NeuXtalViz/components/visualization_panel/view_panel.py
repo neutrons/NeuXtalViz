1dcbe4b9399d5caf442dc30cc7efa85a93d2e469
Yakubov, Sergey <yakubovs@ornl.gov>
2025-12-17 13:28:46 -0500

    gui for vis panel

--- src/NeuXtalViz/components/visualization_panel/view_panel.py
+++ src/NeuXtalViz/components/visualization_panel/view_panel.py
@@ -1,11 +1,6 @@
-import numpy as np
-import pyvista as pv
-from io import BytesIO
-
-from nova.mvvm.panel_binding import PanelBinding
-
 import panel as pn
-
+import pyvista as pv
+from nova.mvvm.panel_binding import PanelBinding
 
 from NeuXtalViz.components.visualization_panel.view_model import VizViewModel
 from NeuXtalViz.views.shared.base_plotter import BasePlotter
@@ -20,15 +15,99 @@ class VisualizationPanel:
         self.plotter = BasePlotter(pv_plotter)
         self.create_ui()
 
-
     def create_ui(self):
         plotter = self.plotter.pv_plotter
-        geo_pan_pv = pn.panel(plotter.ren_win)
-        self._layout = pn.Row(geo_pan_pv)
+        pvcylinder = pv.Cylinder(resolution=8, direction=(0, 1, 0))
+        cylinder_actor = plotter.add_mesh(pvcylinder, color=(1, 0.2, 0.4), smooth_shading=True)
+        cylinder_actor.RotateX(30.0)
+        cylinder_actor.RotateY(-45.0)
+        geo_pan_pv = pn.panel(plotter.ren_win, sizing_mode='stretch_both')
+        plotter.reset_camera()
+
+        head = pn.GridSpec(sizing_mode='stretch_width')
+        head[0, 0] = pn.Column(
+            pn.widgets.Button(name='Save Screenshot', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='Reset View', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='Reset Camera', sizing_mode='stretch_width')
+        )
+        dir_view = pn.GridBox(
+            pn.widgets.Button(name='+Qx', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='+Qy', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='+Qz', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='a*', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='b*', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='c*', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='-Qx', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='-Qy', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='-Qz', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='a', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='b', sizing_mode='stretch_width'),
+            pn.widgets.Button(name='c', sizing_mode='stretch_width'),
+            ncols=6,
+            sizing_mode='stretch_width'
+        )
+        man_view = pn.Column(
+            pn.Row(
+                pn.widgets.Select(name='Axis Type', options=['hkl', 'uvw'], sizing_mode='stretch_width'),
+                pn.widgets.Select(name='Up Axis Type', options=['hkl', 'uvw'], sizing_mode='stretch_width')
+            ),
+            pn.GridBox(
+                pn.Row(
+                    pn.widgets.FloatInput(name='h', sizing_mode='stretch_width'),
+                    pn.widgets.FloatInput(name='k', sizing_mode='stretch_width'),
+                    pn.widgets.FloatInput(name='l', sizing_mode='stretch_width'),
+                    pn.widgets.Button(name='View Axis', align='center'),
+                ),
+                pn.Row(
+                    pn.widgets.FloatInput(name='h', sizing_mode='stretch_width'),
+                    pn.widgets.FloatInput(name='k', sizing_mode='stretch_width'),
+                    pn.widgets.FloatInput(name='l', sizing_mode='stretch_width'),
+                    pn.widgets.Button(name='View Up Axis', align='center'),
+                ),
+                ncols=2)
+        )
+        head[0, 1:4] = pn.Tabs(('Direction View', dir_view), ('Manual View', man_view))
+
+        head[0, 4] = pn.Column(
+            pn.widgets.Checkbox(name='Reciprocal Lattice'),
+            pn.widgets.Checkbox(name='Show Axes'),
+            pn.widgets.Checkbox(name='Parallel Projection')
+        )
+        lp = pn.Column(
+            pn.Row(
+                pn.widgets.FloatInput(name='a', sizing_mode='stretch_width'),
+                pn.widgets.FloatInput(name='b', sizing_mode='stretch_width'),
+                pn.widgets.FloatInput(name='c', sizing_mode='stretch_width'),
+                pn.pane.Str("Å", align='center'),
+            ),
+            pn.Row(
+                pn.widgets.FloatInput(name='α', sizing_mode='stretch_width'),
+                pn.widgets.FloatInput(name='β', sizing_mode='stretch_width'),
+                pn.widgets.FloatInput(name='γ', sizing_mode='stretch_width'),
+                pn.pane.Str("°", align='center'),
+            )
+        )
+        so = pn.Column(
+            pn.Row(
+                pn.pane.Str("u:", align='center'),
+                pn.widgets.FloatInput(sizing_mode='stretch_width'),
+                pn.widgets.FloatInput(name='', sizing_mode='stretch_width'),
+                pn.widgets.FloatInput(name='', sizing_mode='stretch_width'),
+            ),
+            pn.Row(
+                pn.pane.Str("v:", align='center'),
+                pn.widgets.FloatInput(sizing_mode='stretch_width'),
+                pn.widgets.FloatInput(sizing_mode='stretch_width'),
+                pn.widgets.FloatInput(sizing_mode='stretch_width'),
+            )
+        )
+        footer = pn.Tabs(('Lattice Parameters', lp), ('Sample Orientation', so))
+        progress = pn.widgets.Tqdm(sizing_mode='stretch_width')
+
+        self._layout = pn.Column(head, geo_pan_pv, footer, progress)
 
     def __panel__(self):
         return self._layout
-
 
     def update_view(self, event):
         pass
@@ -60,7 +139,6 @@ class VisualizationPanel:
     def show_axes(self, data):
         self.plotter.show_axes(data)
         self.update_view(None)
-
 
     def change_projection(self, parallel_projection):
         """
